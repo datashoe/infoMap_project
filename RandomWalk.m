3d7d4f70190e3c9ac14795d3ce7307d8e55c66d2
lxing532 <lxing532@gmail.com>
2016-11-17 16:28:34 -0700

    RandomWalk update

--- RandomWalk.m
+++ RandomWalk.m
@@ -51,6 +51,36 @@ for i = 1:nodeNum
         pofstep(i,:) = AM(i,:)/wout(i);
     end
 end
-    
+
+%compute edge rate
+edge_rate = zeros(nodeNum,nodeNum);
+for i = 1:nodeNum
+    for j = 1:nodeNum
+        edge_rate(j,i) = edge_rate(j,i) + pofstep(j,i)*p(j);
+    end
+end
+sumofedge = sum(sum(edge_rate));
+edge_rate = edge_rate/sumofedge;
+
+%flow
+num = nnz(edge_rate);
+flow = zeros(num,3);
+count = 1;
+fw = 0;
+for i = 1:nodeNum
+    for j = 1:nodeNum
+        if edge_rate(j,i) ~= 0
+            fw = edge_rate(j,i);
+            flow(count,1) = j;
+            flow(count,2) = i;
+            flow(count,3) = fw;
+            count = count+1;
+        end
+    end
+end
+count = count-1;
+for i = 1:count
+    flow(i,3) = flow(i,3)*(1-alpha);
+end
     
 
